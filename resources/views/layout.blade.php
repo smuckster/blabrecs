84866014815acc7ad7a2a68ac50542766fa1351e
Sam Smucker <sam.smucker@gmail.com>
2020-12-14 19:10:51 -0500

    Install Laravel and whip up basic lobby page

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blabrecs</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    @yield('content')
+
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="app.js"></script>
+</body>
+</html>
---
+++ resources/views/lobby.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('content')
+
+<div class="lobby-container">
+
+    <h1>Blabrecs</h1>
+
+    <p class="instructions">Start a new game and invite other players to join<br> or join a game that's already in progress.</p>
+
+    <button class="btn new-game">Start a game</button>
+
+    <p class="instructions">To join a game, enter the game code and your player code,<br> then click the "Join a game" button.</p>
+
+    <form class="join-game-form" method="get" target="/signin">
+        <label for="game-code">Game Code:</label>
+        <input type="text" class="join-input" id="game-code" name="game" required>
+
+        <br>
+
+        <label for="player-code">Player Code:</label>
+        <input type="text" class="join-input" id="player-code" name="player" required>
+
+        <input type="submit" class="btn join-game" value="Join a game">
+    </form>
+
+
+</div>
+
+@endsection
